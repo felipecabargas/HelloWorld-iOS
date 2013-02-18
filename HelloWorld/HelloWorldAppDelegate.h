d293ed9db6db8a55c3331ed2c7e3e0023ad1fc9b
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2013-02-17 22:49:18 -0300

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by @juanpintoduran on 17-02-13.
+//  Copyright (c) 2013 CABARGASdotCOM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by @juanpintoduran on 17-02-13.
+//  Copyright (c) 2013 CABARGASdotCOM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
